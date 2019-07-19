0c4705adc9802e4edf615a0f6ccdc4e691b0b435
hone1er <josephmvillavicencio@gmail.com>
2019-07-18 22:30:48 -0700

    stacked bar chart working

--- app.py
+++ app.py
@@ -1,16 +1,37 @@
-from flask import Flask, render_template, request, Response, redirect
-
+from flask import Flask, render_template, request, Response, redirect, jsonify
+import pandas as pd
+import os
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template ('index.html')
+    return render_template('index.html')
+
 
 @app.route('/about')
 def about():
-    return render_template ('about.html')
+    return render_template('about.html')
 
+# API call for bar chart
+class Grapher:
+    def __init__(self):
+        file = os.path.join(os.path.dirname('__file__'),
+        "resources/movieGraph.csv")
+        self.df = pd.read_csv(file,index_col=['original_title','title'])
+
+
+    def graph(self, movie):
+        # Format the name
+        self.movie = movie.title()
+        movieInfo = self.df.loc[self.movie]
+        return movieInfo.to_json()
+
+
+grapher = Grapher()
+app.add_url_rule("/graph/<movie>", "",
+                 lambda movie: grapher.graph(movie), methods=["GET"])
 
 if __name__ == "__main__":
     app.run(port=5000)
--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,40 @@
 AOS.init();
+
+function buildLineGraph() {
+    d3.event.preventDefault();
+    // Grab the value from the form and make a d3.json request to the API in order to build traces and layout for line graph
+    var selector = d3.select("#movie");
+    var text = selector.property("value");
+    var url = `/graph/${text}`;
+    d3.json(url).then(function(response){
+        console.log(response.budget[text])
+        var trace1 = {
+            x: [text],
+            y: [response.budget[text]],
+            name: 'Budget',
+            type: 'bar',
+            marker: {
+                color: '#990d0d'
+            }
+        };
+        
+        var trace2 = {
+            x: [text],
+            y: [response.revenue[text]],
+            name: 'Revenue',
+            type: 'bar',
+            marker: {
+                color: '#c5b358'
+              }
+        };
+        
+        var data = [trace1, trace2];
+    
+        var layout = {barmode: 'stack'};
+        
+        Plotly.newPlot('myDiv', data, layout, {responsive: true});
+    });
+};
+
+var filterbtn = d3.select("#filter-btn");
+filterbtn.on("click", buildLineGraph);
