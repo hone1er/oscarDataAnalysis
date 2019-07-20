77c9c68409df6554ef0dc205871b63e4a2a2f84f
hone1er <josephmvillavicencio@gmail.com>
2019-07-19 18:16:46 -0700

    added table under barchart

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-function buildLineGraph() {
+function buildBarChart() {
     d3.event.preventDefault();
     // Grab the value from the form and make a d3.json request to the API in order to build traces and layout for line graph
     var selector = d3.select("#movie");
@@ -11,7 +11,6 @@ function buildLineGraph() {
     .join(' ');
     var url = `/graph/${text}`;
     d3.json(url).then(function(response){
-        console.log(response.budget[text])
         var trace1 = {
             x: [text],
             y: [response.budget[text]],
@@ -45,5 +44,48 @@ function buildLineGraph() {
     });
 };
 
+// Creates the table below the bar chart showing the stats for the movie
+function addTable() {
+    d3.event.preventDefault();
+    // Check if the table already exists and remove it if it does.
+    if (document.contains(document.getElementById("tabled"))) {
+        document.getElementById("tabled").remove();
+}
+    var table = d3.select("#table")
+    .append("table")
+    .attr("id", "tabled");
+    var tbody = table.append("tbody");
+    var selector = d3.select("#movie");
+    var text = selector.property("value");
+    text = text.toLowerCase()
+    .split(' ')
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(' ');
+    var url = `/graph/${text}`;
+
+    d3.json(url).then(function(data){
+        console.log(data.vote_average[text])
+        var header = table.append("thead").append("tr");
+        header
+                .selectAll("th")
+                .data(["Release Date", "Budget", "Revenue", "Popularity", "Vote Average", "Vote Count"])
+                .enter()
+                .append("th")
+                .text(function(d) { return d; });
+        var row = tbody.append("tr");
+        Object.entries(data).forEach(([key, value]) => {
+            console.log(key, value)
+            var cell = tbody.append("td");
+            cell.text(value[text]);
+    });
+});
+}
+
+
+function doAll() {
+    buildBarChart();
+    addTable();
+};
+
 var filterbtn = d3.select("#filter-btn");
-filterbtn.on("click", buildLineGraph);
+filterbtn.on("click", doAll);
