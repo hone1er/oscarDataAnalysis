d2053f7a9c00d2d2345eb8ce06818e42178c5e34
hone1er <josephmvillavicencio@gmail.com>
2019-07-30 11:33:25 -0700

    Created csv for heatmap

--- static/js/dataPlotly.js
+++ static/js/dataPlotly.js
@@ -1,36 +1,59 @@
 Plotly.d3.csv(
-    "femaleActressGenresDF.csv",
-    function(err, rows) {
-      function unpack(rows, key) {
-        return rows.map(function(row) {
-          return row[key];
-        });
-      }
-  console.log(rows)
+  "https://raw.githubusercontent.com/hone1er/oscarDataAnalysis/joe/resources/femaleActressGenresDF.csv",
+  function(err, rows) {
+    function unpack(rows, key) {
+      return rows.map(function(row) {
+        return row[key];
+      });
     }
-  )
-  
-  var data = [
-    {
-      type: 'bar',
-      x: ['2016','2017','2018'],
-      y: [500,600,700],
-      base: [-500,-600,-700],
-      marker: {
-        color: 'red'
+
+    var trace1 = {
+      type: "bar",
+      x: [],
+      y: [],
+      name: "Non-nominees",
+      marker: { color: "#58508d" }
+    };
+
+    var trace2 = {
+      type: "bar",
+      x: [],
+      y: [],
+      name: "Nominees",
+      marker: { color: "#ffa600" }
+    };
+    var counter = 0;
+    rows.forEach(Element => {
+      counter++;
+      Object.entries(Element).forEach(([key, value]) => {
+        if (key.length > 0 && counter === 1) {
+          console.log(key, value, "first");
+          trace1.y.push(value * 100);
+        } else if (key.length > 0 && counter === 2) {
+          console.log(key, value, "second");
+          trace1.x.push(key);
+          trace2.x.push(key);
+          trace2.y.push(value * 100);
+        }
+      });
+    });
+
+    var data = [trace1, trace2];
+    var layout = {
+      barmode: "stack",
+      title: "Percentage by genres and nominations",
+      yaxis: {
+        title: "percentage (#genres/#movies)"
       },
-      name: 'expenses'
-    },
-    {
-      type: 'bar',
-      x: ['2016','2017','2018'],
-      y: [300,400,700],
-      base: 0,
-      marker: {
-        color: 'blue'
+      xaxis: {
+        tickangle: 45
       },
-      name: 'revenue'
-    }]
-  
-  Plotly.newPlot('myDiv', data);
-  
+      paper_bgcolor: "#f3f3f3",
+      plot_bgcolor: "rgba(0, 0, 250, .02)"
+    };
+
+    Plotly.newPlot("myDiv", data, layout, { responsive: true });
+  }
+);
+
+
