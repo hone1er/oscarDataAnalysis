51494076878ad3b49d7aacf8c8d3b665c2128fa6
hone1er <josephmvillavicencio@gmail.com>
2019-07-29 18:11:55 -0700

    added the front end code to the master

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
-from flask import Flask, render_template, request, Response, redirect
-
+from flask import Flask, render_template, request, Response, redirect, jsonify
+import pandas as pd
+import os
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template ('index.html')
+    return render_template('index.html')
 
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/intro')
+def intro():
+    return render_template('intro.html')
+
+# API call for bar chart and tables
+###############################################################
+class Grapher:
+    def __init__(self):
+        file = os.path.join(os.path.dirname('__file__'),
+                            "resources/movieGraph2.csv")
+        self.df = pd.read_csv(file, index_col=['original_title', 'title'])
+
+    def graph(self, movie):
+        # Format the name
+        self.movie = movie
+        movieInfo = self.df.loc[self.movie]
+        return movieInfo.to_json()
+################################################################
+
+
+grapher = Grapher()
+app.add_url_rule("/graph/<movie>", "",
+                 lambda movie: grapher.graph(movie), methods=["GET"])
 
 if __name__ == "__main__":
     app.run(port=5000)
